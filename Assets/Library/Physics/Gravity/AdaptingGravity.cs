ddab1890d6ebae873381973d954f0f487c0bf200
Johannes Deml <send@johannesdeml.com>
2015-06-03 15:19:28 +0200

    ADD move script to player

--- Assets/Library/Physics/Gravity/AdaptingGravity.cs
+++ Assets/Library/Physics/Gravity/AdaptingGravity.cs
@@ -1,65 +1,68 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-[RequireComponent(typeof(Rigidbody))]
-public class AdaptingGravity : MonoBehaviour {
-    [SerializeField]
-    private float gravityStrength = 15f;
-    [SerializeField]
-    private float gravityCheckDistance = 5f;
-    [SerializeField]
-    private float groundCheckDistance = 0.05f;
-
-    private Vector3 groundNormal;
-    private Vector3 gravityDirection = Vector3.down;
-    private float groundDistance = 0f;
-    private new Rigidbody rigidbody;
-    private bool onGround;
+namespace Deml.Physics.Gravity
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class AdaptingGravity : MonoBehaviour {
+        public bool OnGround { get; private set; }
+        public Vector3 GroundNormal { get; private set; }
+        [SerializeField]
+        private float gravityStrength = 15f;
+        [SerializeField]
+        private float gravityCheckDistance = 5f;
+        [SerializeField]
+        private float groundCheckDistance = 0.05f;
     
-    void Awake () {
-        rigidbody = GetComponent<Rigidbody>();
-        groundNormal = Vector3.up;
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        CheckGroundStatus();
-        if (Mathf.Abs(gravityStrength) > 0.02f)
-        {
-            rigidbody.AddForce(gravityDirection * gravityStrength, ForceMode.Force);
+        private Vector3 gravityDirection = Vector3.down;
+        private float groundDistance = 0f;
+        private new Rigidbody rigidbody;
+    
+    
+        void Awake () {
+            rigidbody = GetComponent<Rigidbody>();
+            GroundNormal = Vector3.up;
         }
-	}
-
-    void CheckGroundStatus()
-    {
-        RaycastHit hitInfo;
-#if UNITY_EDITOR
-        // helper to visualise the ground check ray in the scene view
-        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (gravityDirection * gravityCheckDistance), Color.green);
-#endif
-        onGround = false;
-        // 0.1f is a small offset to start the ray from inside the character
-        // it is also good to note that the transform position in the sample assets is at the base of the character
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), gravityDirection, out hitInfo, gravityCheckDistance))
-        {
-            if(hitInfo.transform.tag == "Ground")
+	
+        // Update is called once per frame
+        void FixedUpdate () {
+            CheckGroundStatus();
+            if (Mathf.Abs(gravityStrength) > 0.02f)
             {
-                groundNormal = hitInfo.normal;
-                gravityDirection = -groundNormal;
-                groundDistance = hitInfo.distance - 0.1f;
-                if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), gravityDirection, out hitInfo, groundCheckDistance + 0.1f))
+                rigidbody.AddForce(gravityDirection * gravityStrength, ForceMode.Force);
+            }
+        }
+
+        void CheckGroundStatus()
+        {
+            RaycastHit hitInfo;
+#if UNITY_EDITOR
+            // helper to visualise the ground check ray in the scene view
+            Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (gravityDirection * gravityCheckDistance), Color.green);
+#endif
+            OnGround = false;
+            // 0.1f is a small offset to start the ray from inside the character
+            // it is also good to note that the transform position in the sample assets is at the base of the character
+            if (UnityEngine.Physics.Raycast(transform.position + (Vector3.up * 0.1f), gravityDirection, out hitInfo, gravityCheckDistance))
+            {
+                if(hitInfo.transform.tag == "Ground")
                 {
-                    if (hitInfo.transform.tag == "Ground")
+                    GroundNormal = hitInfo.normal;
+                    gravityDirection = -GroundNormal;
+                    groundDistance = hitInfo.distance - 0.1f;
+                    if (UnityEngine.Physics.Raycast(transform.position + (Vector3.up * 0.1f), gravityDirection, out hitInfo, groundCheckDistance + 0.1f))
                     {
-                        onGround = true;
+                        if (hitInfo.transform.tag == "Ground")
+                        {
+                            OnGround = true;
+                        }
                     }
                 }
             }
         }
-    }
 
-    public void ChangeGravityStrength(float newGravity)
-    {
-        gravityStrength = newGravity;
+        public void ChangeGravityStrength(float newGravity)
+        {
+            gravityStrength = newGravity;
+        }
     }
 }
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using Deml.Physics.Gravity;
+
+[System.Flags]
+public enum PlayerState
+{
+    Initialized = 1 << 0,
+    Movable = 1 << 1,
+    Turnable = 1 << 2
+}
+
+[RequireComponent(typeof(AdaptingGravity))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 0.5f;
+    [SerializeField] private float movementSpeed = 5f;
+    public PlayerState State { get; private set; }
+    private AdaptingGravity gravityController;
+    private new Rigidbody rigidbody;
+	// Use this for initialization
+	void Awake ()
+	{
+	    gravityController = GetComponent<AdaptingGravity>();
+	    rigidbody = GetComponent<Rigidbody>();
+        State = PlayerState.Initialized;
+        EnableState(PlayerState.Movable);
+        EnableState(PlayerState.Turnable);
+	}
+
+    public void Move(Vector3 movementInput, float rotationInput, bool jumpingInput)
+    {
+
+        if (!HasState(PlayerState.Initialized))
+        {
+            return;
+        }
+
+        if (HasState(PlayerState.Turnable))
+        {
+            UpdateRotation(rotationInput);
+        }
+
+        if (HasState(PlayerState.Movable))
+        {
+            UpdateMovement(movementInput, jumpingInput);
+        }
+
+    }
+
+    private void UpdateMovement(Vector3 movementInput, bool jumping)
+    {
+        Debug.Log(movementInput);
+        Vector3 mappedMovement = movementInput.x * transform.right + movementInput.z * transform.forward;
+        float groundModifier = (gravityController.OnGround) ? 1.0f : 0.2f;
+
+        mappedMovement = Vector3.ProjectOnPlane(mappedMovement, gravityController.GroundNormal);
+        mappedMovement *= groundModifier*movementSpeed*Time.fixedDeltaTime;
+        MoveRelative(mappedMovement);
+    }
+
+    private void MoveRelative(Vector3 relativeChange)
+    {
+        rigidbody.MovePosition(transform.position + relativeChange);
+    }
+
+    private void UpdateRotation(float angle)
+    {
+        transform.Rotate(transform.up, angle * 1000f * mouseSensitivity * Time.fixedDeltaTime);
+    }
+
+
+    public void DisableState(PlayerState stateToDisable)
+    {
+        State &= ~stateToDisable;
+    }
+
+    public void EnableState(PlayerState stateToEnable)
+    {
+        State |= stateToEnable;
+    }
+
+    public bool HasState(PlayerState stateToCheck)
+    {
+        return ((State & stateToCheck) == stateToCheck);
+    }
+}
