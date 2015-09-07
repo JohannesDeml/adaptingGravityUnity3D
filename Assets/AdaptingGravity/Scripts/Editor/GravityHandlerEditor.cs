7468f5dcaa5f4f14aacf3b5665c5fcabc77390e9
Johannes Deml <send@johannesdeml.com>
2015-09-07 17:40:01 +0200

    ADD code documentation to gravity handler editor and player input

--- Assets/AdaptingGravity/Scripts/Editor/GravityHandlerEditor.cs
+++ Assets/AdaptingGravity/Scripts/Editor/GravityHandlerEditor.cs
@@ -15,6 +15,10 @@ namespace AdaptingGravity.Editor
     using AdaptingGravity.Physics.Gravity;
     using UnityEditor;
 
+    /// <summary>
+    /// This editor script extends the tweaking functionality for the gravity handler in the editor.
+    /// It adds the possibility to add gravity handles <see cref="GravityHandle"/>
+    /// </summary>
     [CustomEditor(typeof(GravityHandler))]
     public class GravityHandlerEditor : Editor
     {
@@ -26,18 +30,30 @@ namespace AdaptingGravity.Editor
 
             if (GUILayout.Button("Add Handle"))
             {
-                int newHandleID = gravityHandler.handles.Count;
-                GameObject newHandle = (GameObject)Instantiate(gravityHandler.GravityHandlePrefab, gravityHandler.GravityHandles.transform.position,
-                    gravityHandler.GravityHandles.transform.rotation);
-                newHandle.transform.SetParent(gravityHandler.GravityHandles.transform, true);
-                newHandle.name = "Handle" + newHandleID;
+                GameObject newHandle = CreateNewGravityHandle();
                 GravityHandle gravityHandle = newHandle.GetComponent<GravityHandle>();
                 gravityHandle.Parent = gravityHandler;
                 gravityHandler.handles.Add(gravityHandle);
                 EditorUtility.SetDirty(gravityHandler);
                 EditorUtility.SetDirty(newHandle);
+                //Jump to the newly created object in the editor, this way it can be edited right away
                 Selection.activeGameObject = newHandle;
             }
+        }
+
+        /// <summary>
+        /// Creates a new gameobject from a prefab which will be used as a new handle.
+        /// The game object gets the name "Handle" + the number of handles that handle list has stored
+        /// </summary>
+        /// <returns>A gravity handle game object</returns>
+        private GameObject CreateNewGravityHandle()
+        {
+            int newHandleID = gravityHandler.handles.Count;
+            GameObject newHandle = (GameObject)Instantiate(gravityHandler.GravityHandlePrefab, gravityHandler.GravityHandles.transform.position,
+                gravityHandler.GravityHandles.transform.rotation);
+            newHandle.transform.SetParent(gravityHandler.GravityHandles.transform, true);
+            newHandle.name = "Handle" + newHandleID;
+            return newHandle;
         }
     }
 
